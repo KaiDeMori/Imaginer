9c4ba3f8cf76366027fa65b37d7a49a33835f31e
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-06-30 22:56:22 +0200

    feature: 🚀 enabled mipmapping and trilinear filtering for ultra-smooth infinity zoom! No more flicker, just pure buttery visuals. #graphicswin

--- intro_trials/planetwise/webgl/infinity_zoom_webgl_engine.js
+++ intro_trials/planetwise/webgl/infinity_zoom_webgl_engine.js
@@ -74,12 +74,13 @@ function setup_textured_quad_buffer(gl, prog) {
 function create_texture_from_image(gl, image) {
    const tex = gl.createTexture();
    gl.bindTexture(gl.TEXTURE_2D, tex);
-   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+   gl.generateMipmap(gl.TEXTURE_2D);
    return tex;
 }
 
