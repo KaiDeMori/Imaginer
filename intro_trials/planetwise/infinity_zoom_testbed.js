32291bcaba0561b07d7cd6b4de65cf42bfc6e335
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-07-01 20:42:56 +0200

    feat: update image preloading to include specific folder for zoom images

--- intro_trials/planetwise/infinity_zoom_testbed.js
+++ intro_trials/planetwise/infinity_zoom_testbed.js
@@ -1,7 +1,20 @@
 const IMAGE_FOLDER = 'zoom_images';
 
+// const LAYERS_DATA = [
+//    { zoom: 25, image: '60_alien_island.png' },
+//    { zoom: 25, image: '70_alien_forest.png' },
+//    { zoom: 25, image: '80_alien_village.png' },
+//    { zoom: 25, image: '90_alien_hut.png' },
+//    { zoom: 25, image: '100_alien_closeup.png' },
+// ];
+
 const LAYERS_DATA = [
-   { zoom: 25, image: '60_alien_island.png' },
+   { zoom: 25, image: '10_new_planete.png' },
+   { zoom: 25, image: '20_alien_island_II_tricky_transition_continental_B.png' },
+   { zoom: 25, image: '30_alien_island_II_tricky_transition.png' },
+   { zoom: 25, image: '40_alien_island_II_atoll_tiny_land.png' },
+   { zoom: 25, image: '50_alien_island_II_atoll.png' },
+   { zoom: 25, image: '60_alien_island_II.png' },
    { zoom: 25, image: '70_alien_forest.png' },
    { zoom: 25, image: '80_alien_village.png' },
    { zoom: 25, image: '90_alien_hut.png' },
