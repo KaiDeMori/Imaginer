9353480efc85e59e33eacf0791c1008f0c3c5124
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-07-01 19:54:28 +0200

    fix: correct zoom level for alien island II atoll layer in LAYERS_DATA

--- intro_trials/planetwise/webgl/example LAYERS_DATAs.js
+++ intro_trials/planetwise/webgl/example LAYERS_DATAs.js
@@ -14,7 +14,7 @@ const LAYERS_DATA = [
    { zoom: 25, image: '10_new_planete.png' },
    { zoom: 25, image: '20_alien_island_II_tricky_transition_continental_B.png' },
    { zoom: 25, image: '30_alien_island_II_tricky_transition.png' },
-   { zoom: 50, image: '40_alien_island_II_atoll_tiny_land.png' },
+   { zoom: 25, image: '40_alien_island_II_atoll_tiny_land.png' },
    { zoom: 25, image: '50_alien_island_II_atoll.png' },
    { zoom: 25, image: '60_alien_island_II.png' },
    { zoom: 25, image: '70_alien_forest.png' },
