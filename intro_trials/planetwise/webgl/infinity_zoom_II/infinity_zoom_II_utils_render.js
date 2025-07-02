018666cc1b93072f4fa2e82badbfb43e7fbdb879
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-07-02 23:52:10 +0200

    fix: enable mipmapping for improved texture rendering quality

--- intro_trials/planetwise/webgl/infinity_zoom_II/infinity_zoom_II_utils_render.js
+++ intro_trials/planetwise/webgl/infinity_zoom_II/infinity_zoom_II_utils_render.js
@@ -3,10 +3,11 @@ function upload_texture(gl, layer) {
    const tex = gl.createTexture();
    gl.bindTexture(gl.TEXTURE_2D, tex);
    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, layer.image);
-   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+   gl.generateMipmap(gl.TEXTURE_2D);
    layer.texture = tex;
    layer.loaded = true;
 }
