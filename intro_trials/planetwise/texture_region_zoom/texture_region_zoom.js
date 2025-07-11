5478caded97f5817805ffd24ed34aa8191469da3
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-07-11 04:53:08 +0200

    finalprevious

--- intro_trials/planetwise/texture_region_zoom/texture_region_zoom.js
+++ intro_trials/planetwise/texture_region_zoom/texture_region_zoom.js
@@ -35,6 +35,7 @@ window.infinity_zoom_II.texture_region_zoom = (function () {
   let config = null;
 
   let final_layer = null;
+  let previous_layer = null;
 
   function build_matrices(w, h) {
     const proj = mat_ortho(w, h);
@@ -125,15 +126,14 @@ window.infinity_zoom_II.texture_region_zoom = (function () {
     config = window.infinity_zoom_II.config.region_zoom;
     on_complete_callbacks = on_complete;
     final_layer = _final_layer;
+    previous_layer = _previous_layer;
 
     initial_rotation = previous_rotation;
     log("initial_rotation", initial_rotation);
 
     const vs_src = `precision mediump float;attribute vec2 a_position;attribute vec2 a_tex;uniform mat3 u_matrix;varying vec2 v_tex;void main(){vec3 p=u_matrix*vec3(a_position,1.0);gl_Position=vec4(p.xy,0.0,1.0);v_tex=a_tex;}`;
     // No Y-flip: input is always upright
-    // const fs_src = `precision mediump float;varying vec2 v_tex;uniform sampler2D u_texture;void main(){gl_FragColor=texture2D(u_texture,v_tex);}`;
-    // DEBUG: Make final image half transparent
-    const fs_src = `precision mediump float;varying vec2 v_tex;uniform sampler2D u_texture;void main(){vec4 c=texture2D(u_texture,v_tex);gl_FragColor=vec4(c.rgb,c.a*0.5);}`;
+    const fs_src = `precision mediump float;varying vec2 v_tex;uniform sampler2D u_texture;void main(){gl_FragColor=texture2D(u_texture,v_tex);}`;
     const vs = gl_ctx.createShader(gl_ctx.VERTEX_SHADER);
     gl_ctx.shaderSource(vs, vs_src);
     gl_ctx.compileShader(vs);
