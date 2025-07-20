6454f801bea17ba9848cfb6f29a6085db11dfc95
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-07-20 23:26:39 +0200

    purged

--- intro_trials/planetwise/infinity_zoom_II/infinity_zoom_preloader.js
+++ intro_trials/planetwise/infinity_zoom_II/infinity_zoom_preloader.js
@@ -1,81 +1,4 @@
 // Image preloader module for infinity zoom
-let images = [];
-let mystery_image = null;
-let images_loaded = false;
-let image_load_callbacks = [];
-
-function preload_images(layer_data, image_folder = "zoom_images") {
-  if (images_loaded) {
-    log("[preload_images] Images already loaded, skipping.");
-    return; // Prevent double loading
-  }
-  let loaded = 0;
-  const total = layer_data.length + 1; // +1 for mystery image
-  images = new Array(layer_data.length);
-  if (layer_data.length === 0) {
-    images_loaded = true;
-    log("[preload_images] No images to load.");
-    image_load_callbacks.forEach((cb) => cb(images, mystery_image));
-    image_load_callbacks = [];
-    return;
-  }
-  log(`[preload_images] Loading ${total} images (including mystery)...`);
-
-  // Load mystery image first
-  const mystery_img = new Image();
-  mystery_img.onload = () => {
-    mystery_image = mystery_img;
-    loaded++;
-    log(`[preload_images] [${loaded}/${total}] loaded mystery image (${mystery_img.width}x${mystery_img.height})`);
-    if (loaded === total) {
-      images_loaded = true;
-      log(`[preload_images] All ${total} images loaded!`);
-      image_load_callbacks.forEach((cb) => cb(images, mystery_image));
-      image_load_callbacks = [];
-    }
-  };
-  mystery_img.onerror = (e) => {
-    log(`[preload_images] ERROR loading mystery image`);
-  };
-  mystery_img.src = window.infinity_zoom_II.config.MYSTERY_IMAGE;
-  log(`[preload_images] Started loading mystery image`);
-
-  // Load layer images
-  layer_data.forEach((layer, i) => {
-    const img = new Image();
-    img.onload = () => {
-      images[i] = img;
-      loaded++;
-      // Only log the file name, not the full path
-      const file_name = get_file_name(layer.image);
-      log(`[preload_images] [${loaded}/${total}] loaded: ${file_name} (${img.width}x${img.height})`);
-      if (loaded === total) {
-        images_loaded = true;
-        log(`[preload_images] All ${total} images loaded!`);
-        image_load_callbacks.forEach((cb) => cb(images, mystery_image));
-        image_load_callbacks = [];
-      }
-    };
-    img.onerror = (e) => {
-      const file_name = get_file_name(layer.image);
-      log(`[preload_images] ERROR loading image: ${file_name}`);
-    };
-    img.src = `${image_folder}/${layer.image}`;
-    // Only log the file name, not the full path
-    const file_name = get_file_name(layer.image);
-    log(`[preload_images] Started loading: ${file_name}`);
-  });
-}
-
-function on_images_loaded(callback) {
-  if (images_loaded) {
-    log("[on_images_loaded] Images already loaded, invoking callback immediately.");
-    callback(images, mystery_image);
-  } else {
-    log("[on_images_loaded] Images not yet loaded, queuing callback.");
-    image_load_callbacks.push(callback);
-  }
-}
 
 // Load mystery image only
 function load_mystery_image_only(callback) {
@@ -159,8 +82,6 @@ function get_file_name(path) {
 
 // Attach to unified namespace
 window.infinity_zoom_II.preloader = {
-  preload_images,
-  on_images_loaded,
   load_mystery_image_only,
   load_layer_images_only,
   load_all_images,
