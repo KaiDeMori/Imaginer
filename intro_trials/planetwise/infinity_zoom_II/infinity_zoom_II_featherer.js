16b3d0349423929758fc45766dc9c18a0805a4f5
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-07-23 02:14:40 +0200

    c

---
+++ intro_trials/planetwise/infinity_zoom_II/infinity_zoom_II_featherer.js
@@ -0,0 +1,8 @@
+function process_images_with_feathering(images, feather_size, callback) {
+  // TBD
+  callback(images);
+}
+
+window.infinity_zoom_II.featherer = {
+  process_images_with_feathering,
+};
--- intro_trials/planetwise/infinity_zoom_II/infinity_zoom_II_preloader.js
+++ intro_trials/planetwise/infinity_zoom_II/infinity_zoom_II_preloader.js
@@ -2,11 +2,22 @@ function load_all_images(callback) {
   const layers_data = window.infinity_zoom_II.config.LAYERS_DATA;
   const image_path = window.infinity_zoom_II.config.RELATIVE_IMAGE_PATH;
   const mystery_image_path = window.infinity_zoom_II.config.MYSTERY_IMAGE;
+  const feather_size = window.infinity_zoom_II.config.feather_size;
 
   const layer_images = [];
   let loaded_count = 0;
   const total_count = layers_data.length + 1; // layers + mystery image
   let mystery_image = null;
+
+  function on_all_loaded() {
+    if (feather_size !== undefined) {
+      window.infinity_zoom_II.featherer.process_images_with_feathering(layer_images, feather_size, (processed_images) => {
+        callback(processed_images, mystery_image);
+      });
+    } else {
+      callback(layer_images, mystery_image);
+    }
+  }
 
   // Load layer images
   layers_data.forEach((layer, i) => {
@@ -14,7 +25,7 @@ function load_all_images(callback) {
     img.onload = () => {
       loaded_count++;
       if (loaded_count === total_count) {
-        callback(layer_images, mystery_image);
+        on_all_loaded();
       }
     };
     img.src = `${image_path}/${layer.image}`;
@@ -26,7 +37,7 @@ function load_all_images(callback) {
   mystery_img.onload = () => {
     loaded_count++;
     if (loaded_count === total_count) {
-      callback(layer_images, mystery_image);
+      on_all_loaded();
     }
   };
   mystery_img.src = mystery_image_path;
