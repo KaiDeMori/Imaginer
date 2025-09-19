d3b4ecaba3f1cb38bb650446ecb9af2d3a08ce91
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-09-19 03:44:45 +0200

    moved infinity zoom to intro/04

--- intro/04/infinity_zoom_II_configs.js
+++ intro/04/infinity_zoom_II_configs.js
@@ -5,7 +5,7 @@ if (!window.infinity_zoom_II.region_zoom) window.infinity_zoom_II.region_zoom = 
 // Infinity Zoom II Configuration Module
 
 // Constants originally from HTML file
-window.infinity_zoom_II.config.RELATIVE_IMAGE_PATH = "../zoom_images_planete/webp";
+window.infinity_zoom_II.config.RELATIVE_IMAGE_PATH = "../../assets/ai_universe/zoom_images_planete/webp";
 
 window.infinity_zoom_II.config.LAYERS_DATA = [
   { zoom: 25, image: "10_new_planete_fixed.webp" },
@@ -50,15 +50,15 @@ window.infinity_zoom_II.config.region_zoom = {
 
 // Display images for alien screen portal effect during region zoom
 window.infinity_zoom_II.config.REGION_DISPLAY_IMAGE_PATHS = [
-  "../zoom_images_planete/display_images/region_zoom/u1264212648_photo_of_of_a_traditional_French_mime_riding_a_bicy_bab8869e-91ed-4290-b766-63beb51c844d.jpg",
-  "../zoom_images_planete/display_images/region_zoom/u1264212648_photo_of_a_panda_sitting_on_a_table_in_the_forest_a_6c82a065-dcbe-4937-8d45-8507648d2a5b.jpg",
-  "../zoom_images_planete/display_images/region_zoom/Final_recursion.jpg",
+  "../../assets/ai_universe/zoom_images_planete/display_images/region_zoom/u1264212648_photo_of_of_a_traditional_French_mime_riding_a_bicy_bab8869e-91ed-4290-b766-63beb51c844d.jpg",
+  "../../assets/ai_universe/zoom_images_planete/display_images/region_zoom/u1264212648_photo_of_a_panda_sitting_on_a_table_in_the_forest_a_6c82a065-dcbe-4937-8d45-8507648d2a5b.jpg",
+  "../../assets/ai_universe/zoom_images_planete/display_images/region_zoom/Final_recursion.jpg",
 ];
 
 // Display images for main zoom portal effect
 window.infinity_zoom_II.config.MAIN_DISPLAY_IMAGE_PATHS = [
-  "../zoom_images_planete/display_images/main_zoom/A_unicorn-dinosaur_1749938156.jpg",
-  "../zoom_images_planete/display_images/main_zoom/An_ancient_library_h_1749941586.jpg",
+  "../../assets/ai_universe/zoom_images_planete/display_images/main_zoom/A_unicorn-dinosaur_1749938156.jpg",
+  "../../assets/ai_universe/zoom_images_planete/display_images/main_zoom/An_ancient_library_h_1749941586.jpg",
 ];
 
 /*
