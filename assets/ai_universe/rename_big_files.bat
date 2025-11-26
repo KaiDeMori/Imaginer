04621d3d195ddb405e3950c4d74566a3e4da9198
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2025-11-26 02:31:09 +0100

    .

--- assets/ai_universe/rename_big_files.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Batch script to rename all big_XX.png files in the current folder to XX.png
-for %%f in (big_*.png) do (
-    set "filename=%%~nf"
-    setlocal enabledelayedexpansion
-    set "num=!filename:big_=!"
-    ren "%%f" "!num!.png"
-    endlocal
-)
